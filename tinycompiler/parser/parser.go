090e5f4601b4ef04de43f300f32978efbbf166a0
Mina <wilhelmina.jane@gmail.com>
2019-01-05 20:24:05 +0000

    implements parser function

--- tinycompiler/parser/parser.go
+++ tinycompiler/parser/parser.go
@@ -2,6 +2,67 @@ package tinycompiler
 
 import "github.com/minaorangina/super-tiny-compiler/types"
 
-func Parser(tokens []types.Token) {
+// Parser converts a slice of tokens into its Abstract Syntax Tree (AST) representation
+func Parser(tokens []types.Token) (types.Ast, error) {
+	current := 0
+	var walk func() types.Node
+	walk = func() types.Node {
+		token := tokens[current]
 
+		switch token.TokenType {
+		case "number":
+			current++
+			return types.Node{
+				NodeType: types.NumberLiteral,
+				Value:    token.Value,
+				Params:   nil,
+			}
+		case "string":
+			current++
+			return types.Node{
+				NodeType: types.StringLiteral,
+				Value:    token.Value,
+				Params:   nil,
+			}
+		case "paren":
+			if token.Value == "(" {
+				// skip parenthesis
+				current++
+				token = tokens[current]
+
+				node := types.Node{
+					NodeType: types.CallExpression,
+					Value:    token.Value,
+					Params:   []types.Node{},
+				}
+
+				// first parameter
+				current++
+				token = tokens[current]
+
+				for (token.TokenType != "paren") ||
+					(token.TokenType == "paren" && token.Value != ")") {
+					node.Params = append(node.Params, walk())
+					token = tokens[current]
+				}
+				// skip final parenthesis
+				current++
+				return node
+			}
+		default:
+			panic("unknown token type")
+		}
+
+		return types.Node{}
+	}
+
+	ast := types.Ast{
+		Body: []types.Node{},
+	}
+
+	for current < len(tokens) {
+		ast.Body = append(ast.Body, walk())
+	}
+
+	return ast, nil
 }
--- tinycompiler/parser/parser_test.go
+++ tinycompiler/parser/parser_test.go
@@ -23,18 +23,44 @@ func TestParser(t *testing.T) {
 	expected := types.Ast{
 		Body: []types.Node{
 			types.Node{
-				NodeType: "CallExpression",
+				NodeType: types.CallExpression,
 				Value:    "add",
-				Params:   []Node{},
+				Params: []types.Node{
+					types.Node{types.NumberLiteral, "2", nil},
+					types.Node{types.CallExpression, "subtract", []types.Node{
+						types.Node{types.NumberLiteral, "4", nil},
+						types.Node{types.NumberLiteral, "2", nil},
+					}},
+				},
 			},
 		},
 	}
 
 	result, err := Parser(tokens)
 	if err != nil {
-		return t.Fail()
+		t.Fail()
 	}
-	if reflect.DeepEqual(expected, result) {
-		return t.Errorf("ASTs not equal.\nExpected:\n%+v\nActual:\n%+v\n", expected, result)
+	if !reflect.DeepEqual(expected, result) {
+		t.Errorf("ASTs not equal.\nExpected:\n%+v\nActual:\n%+v\n", expected, result)
 	}
+
+	tokens = []types.Token{
+		types.Token{"paren", "("},
+		types.Token{"name", "add"},
+		types.Token{"notanodetype", "2"},
+		types.Token{"paren", "("},
+		types.Token{"name", "subtract"},
+		types.Token{"number", "4"},
+		types.Token{"number", "2"},
+		types.Token{"alsonotanodetype", ")"},
+		types.Token{"paren", ")"},
+	}
+
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("Failed to panic when unknown NodeType encountered")
+		}
+	}()
+
+	Parser(tokens)
 }
